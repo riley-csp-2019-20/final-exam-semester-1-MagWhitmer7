4f15fd3c88430a833c825705eae37c947c7d8169
MagWhitmer7 <55501077+MagWhitmer7@users.noreply.github.com>
2019-12-19 11:20:34 -0500

    Actually done now

--- 2019fallfinalexamCSP.py
+++ 2019fallfinalexamCSP.py
@@ -1,36 +1,36 @@
-#2019-20 Fall Computer Science Principles Final Exam
+# 2019-20 Fall Computer Science Principles Final Exam
 
-#Ms. Haubold
+# Ms. Haubold
 
 
-#Name
-# Margaret Whitmer
-#Date
-# 12/19/2019
+# Name
+# --Margaret Whitmer--
+# Date
+# --12/19/2019--
 
 
 #### INSTRUCTIONS ####
-#Create an etch a sketch turtle game
-#The turtle should move with the arrow keys (up, down, left and right), and draw
-#Space should clear the screen
-#o and p should make the pen size bigger and smaller
-#u should pick the pen up or put the pen down
-#All code must be commented
-#BONUS
-#Add a feature to change colors
-#
+# Create an etch a sketch turtle game
+# The turtle should move with the arrow keys (up, down, left and right), and draw
+# Space should clear the screen
+# o and p should make the pen size bigger and smaller
+# u should pick the pen up or put the pen down
+# All code must be commented
+# BONUS
+# Add a feature to change colors
+# I did that 
 
-#import required libraries
+# import required libraries
 
 import turtle as trtl
 
-#create turtle
-size = 1
+# create turtle
+size = 1 # set global size variable
 
-drawer = trtl.Turtle()
-drawer.pensize(size)
+drawer = trtl.Turtle() # create turtle (His real name is neb)
+drawer.pensize(size) # set initial pensize
 
-#movement functions
+# movement functions
 
 # go up
 def up():
@@ -56,58 +56,60 @@ def right():
 
 # pensize gets bigger
 def pen_size_big():
-    global size # check current size
-    drawer.pensize(size + 1) # add one to that size
-    size += 1
+  global size # check current size
+  drawer.pensize(size + 1) # add one to that size
+  size += 1 # save changes with the global size variable
 
 # pensize gets smaller
 def pen_size_small():
-   global size # check current size
-   drawer.pensize(size - 1) # subtract one from that size
-   size -= 1
+  global size # check current size
+  drawer.pensize(size - 1) # subtract one from that size
+  size -= 1 # save changes with the global size variable
 
-# make the pen pich up / go down
+# make the pen pick up / place down
 def pen_up_down():
-    if drawer.isdown() == True: # check if the pen is up/down
-        drawer.penup() # if down, pick up
-    else:
-        drawer.pendown() # if up, put down
+  if drawer.isdown() == True: # check if the pen is up/down
+    drawer.penup() # if down, pick up
+  else:
+    drawer.pendown() # if up, put down
 
 # clear the drawing
 def clear_screen():
-    drawer.clear() # clear screen
+  drawer.clear() # clear screen
 
-
+# change the pencolor
 def color_change():
-    color = input("type a color:")
-    drawer. pencolor(color)
+  color = input("type a color:") # set color variable to player response
+  drawer. pencolor(color) # change pencolor to color
 
   
-#create screen
+# create screen
 
 wn = trtl.Screen()
 
-#bind to keypresses
+# bind to keypresses
 
 # arrow keys = move turtle
-wn.onkeypress(up, "Up")
-wn.onkeypress(down, "Down")
-wn.onkeypress(left, "Left")
-wn.onkeypress(right, "Right")
-# o = make bigger, p = make smaller
-wn.onkeypress(pen_size_big, "o")
-wn.onkeypress(pen_size_small, "p")
-# space = clear screen
-wn.onkeypress(clear_screen, "space")
-# u = pen up/down
-wn.onkeypress(pen_up_down, "u")
-# c = change pen color 
-wn.onkeypress(color_change, "c")
+wn.onkeypress(up, "Up") # up = go up
+wn.onkeypress(down, "Down") # down = go down
+wn.onkeypress(left, "Left") # left = go left
+wn.onkeypress(right, "Right") # right = go right
+# change pensize
+wn.onkeypress(pen_size_big, "o") # o = make bigger
+wn.onkeypress(pen_size_small, "p") #p = make smaller
+# clear screen
+wn.onkeypress(clear_screen, "space") # space = clear screen
+# pen up / down
+wn.onkeypress(pen_up_down, "u") # u = pen up/down
+# change color :)
+wn.onkeypress(color_change, "c") # c = change pen color prompt 
 
-#listen
+# listen
 
 wn.listen()
 
-#mainloop
+# mainloop
 
 wn.mainloop()
+
+# Have a good winter break! Merry Christmas!
